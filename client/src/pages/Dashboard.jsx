b56c65b45ec90c8364d9f255681b859a97b2d5a3
ayush931 <ayushkumar9315983@gmail.com>
2025-03-20 17:27:02 +0530

    Buttons and Fonts

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -15,13 +15,149 @@ function Dashboard() {
               className="h-[60px] w-[60px]"
             />
           </h1>
-          <p>Here's What happening on your store today. See the statistics at once.</p>
+          <p>
+            Here's What happening on your store today. See the statistics at
+            once.
+          </p>
           <br />
-          <Button className="btn-green !capitalize !gap-1 flex items-center justify-center"><HiPlusSm className="text-[20px]" />Add product</Button>
+          <Button className="btn-green !capitalize !gap-1 flex items-center justify-center">
+            <HiPlusSm className="text-[20px]" />
+            Add product
+          </Button>
         </div>
-        <img src="/pexels-suzyhazelwood-2536965.jpg" alt="" className="w-[250px]" />
+        <img
+          src="/pexels-suzyhazelwood-2536965.jpg"
+          alt=""
+          className="w-[250px]"
+        />
       </div>
       <DashboardBoxes />
+      <div className="card my-4 shadow-md sm:shadow-lg bg-white">
+        <div className="flex items-center justify-between px-5 py-5">
+          <h2 className="text-[20px] font-[600]">Recent Orders</h2>
+        </div>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table class="w-full text-sm text-left rtl:text-right">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Product name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <div class="flex items-center">
+                    Color
+                    <a href="#">
+                      <svg
+                        class="w-3 h-3 ms-1.5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                      </svg>
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <div class="flex items-center">
+                    Category
+                    <a href="#">
+                      <svg
+                        class="w-3 h-3 ms-1.5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                      </svg>
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <div class="flex items-center">
+                    Price
+                    <a href="#">
+                      <svg
+                        class="w-3 h-3 ms-1.5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                      </svg>
+                    </a>
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  <span class="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-white border-b  border-gray-200">
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">Silver</td>
+                <td class="px-6 py-4">Laptop</td>
+                <td class="px-6 py-4">$2999</td>
+                <td class="px-6 py-4 text-right">
+                  <a
+                    href="#"
+                    class="font-medium text-blue-600 hover:underline"
+                  >
+                    Edit
+                  </a>
+                </td>
+              </tr>
+              <tr class="bg-white border-b border-gray-200">
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">White</td>
+                <td class="px-6 py-4">Laptop PC</td>
+                <td class="px-6 py-4">$1999</td>
+                <td class="px-6 py-4 text-right">
+                  <a
+                    href="#"
+                    class="font-medium text-blue-600 hover:underline"
+                  >
+                    Edit
+                  </a>
+                </td>
+              </tr>
+              <tr class="bg-white">
+                <th
+                  scope="row"
+                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Magic Mouse 2
+                </th>
+                <td class="px-6 py-4">Black</td>
+                <td class="px-6 py-4">Accessories</td>
+                <td class="px-6 py-4">$99</td>
+                <td class="px-6 py-4 text-right">
+                  <a
+                    href="#"
+                    class="font-medium text-blue-600 hover:underline"
+                  >
+                    Edit
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
